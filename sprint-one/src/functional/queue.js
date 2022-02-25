a3e3dcc7139fd4fcd72863a4278dfaabb9fa954b
Andrew <andrew.orodenker@gmail.com>
2022-02-24 16:03:47 -0800

    functional stack and queue passing all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  someInstance.head = 0;
+  someInstance.tail = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance.tail ++;
+    storage[someInstance.tail] = value;
+
   };
 
   someInstance.dequeue = function() {
+    if (someInstance.head <= someInstance.tail) {
+      someInstance.head ++;
+      return storage[someInstance.head];
+    }
   };
 
   someInstance.size = function() {
+    var total = someInstance.tail - someInstance.head;
+    if (total < 0) {
+      total = 0;
+    }
+    return total;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function() {
   var someInstance = {};
+  someInstance.x = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.x] = value;
+    someInstance.x ++;
+
   };
 
   someInstance.pop = function() {
+    someInstance.x --;
+    if (someInstance.x < 0) {
+      someInstance.x = 0;
+    }
+    return storage[someInstance.x];
   };
 
   someInstance.size = function() {
+    return someInstance.x;
+
   };
 
   return someInstance;
