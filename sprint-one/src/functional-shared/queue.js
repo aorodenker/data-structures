9f79dd4e017b5a0e6bca3e3329030212e9bc8744
Andrew <andrew.orodenker@gmail.com>
2022-02-24 20:41:01 -0800

    functional-shared passing all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newThing = {};
+  newThing.head = 0;
+  newThing.tail = 0;
+  newThing.storage = {};
+  _.extend(newThing, queueMethods);
+
+  return newThing;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue(value) {
+    this.tail++;
+    this.storage[this.tail] = value;
+  },
+  dequeue() {
+    if (this.head <= this.tail) {
+      this.head ++;
+    }
+    return this.storage[this.head];
+  },
+  size() {
+    var total = this.tail - this.head;
+    if (total < 0) {
+      total = 0;
+    }
+    return total;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,58 @@
+// var Stack = function() {
+//   var newInstance = {};
+//   var storage = {};
+//   newInstance.x = 0;
+//   _.extend(newInstance, stackMethods);
+//   // use _.extend(first = method delivery target, second = methods from target)
+
+//   return newInstance;
+// };
+
+// //stackMethods stores methods
+// var stackMethods = {};
+// stackMethods.push = function(value) {
+//   storage[newInstance.x] = value;
+//   newInstance.x ++;
+// };
+// stackMethods.pop = function() {
+//   newInstance.x --;
+//   if (newInstance.x < 0) {
+//     newInstance.x = 0;
+//   }
+//   return storage[newInstance.x];
+// };
+// stackMethods.size = function() {
+//   return newInstance.x;
+// };
+
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newInstance = {};
+  newInstance.storage = {};
+  newInstance.x = 0;
+  _.extend(newInstance, stackMethods);
+  // use _.extend(first = method delivery target, second = methods from target)
+
+  return newInstance;
 };
 
-var stackMethods = {};
+//stackMethods stores methods
+var stackMethods = {
+  push(value) {
+    this.storage[this.x] = value;
+    this.x ++;
+  },
+  pop() {
+    this.x --;
+    if (this.x < 0) {
+      this.x = 0;
+    }
+    return this.storage[this.x];
+  },
+  size() {
+    return this.x;
+  }
+};
+
 
 
